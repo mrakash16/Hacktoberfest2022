5aa6cfdd34f9d273122bb8afcb35d6dc2f03d19f
mrakash16 <66001963+mrakash16@users.noreply.github.com>
2022-10-05 02:01:42 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1829,5 +1829,11 @@ contributors = [
     id:377,
     fullname: "Anusha Gupta",
     username: "https://github.com/anushagupta1312",
-  }
+  },
+  {
+    id:378,
+    fullname: "Akash yadab",
+    username: "https://github.com/mrakash16",
+    }
+  
 ];
